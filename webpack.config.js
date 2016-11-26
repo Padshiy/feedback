a71b5c4b449fb66c1e482aacf7b0d93ec9cfeab8
Padshiy <one139@yandex.ru>
2016-11-26 20:22:19 +0700

    issue#1
    issue#1

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const StylExtractTextPlugin = require("extract-text-webpack-plugin");
+const CSSExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    context: __dirname + '/frontend',
+
+    entry: {
+        main: './index'
+    },
+
+    output: {
+        path:       __dirname + '/public',
+        publicPath: '/',
+        filename:   '[name].js'
+    },
+
+    module: {
+
+        loaders: [{
+            test:    /\.js$/,
+            include: __dirname + '/frontend',
+            loader:  "babel?presets[]=es2015"
+        }, {
+            test:   /\.pug$/,
+            loader: "pug"
+        }, {
+            test:   /\.styl$/,
+            loader: StylExtractTextPlugin.extract('css!stylus?resolve url')
+        }, {
+            test:   /\.css/,
+            loader: 'style!css!autoprefixer?browsers=last 2 versions'
+        }, {
+            test:   /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+            loader: 'file?name=[path][name].[ext]'
+        }]
+
+    },
+
+    plugins: [
+        new StylExtractTextPlugin('css/[name].[hash].css', {allChunks: true}),
+        new CSSExtractTextPlugin('css/[name].[hash].css', {allChunks: true}),
+        new webpack.NoErrorsPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './index.pug'
+        })
+    ]
+};
